9b41509c5472d52278d4e2c50ede79703d8ed6a4
adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-02-05 15:12:07 +0100

    Session 5 exercices

---
+++ session_5/ex-1.py
@@ -0,0 +1,14 @@
+def count_a(seq):
+    #counting the number of A's in the sequence
+
+    #Counter for the A's
+    result = 0
+    for b in seq:
+        if b = 'A':
+            result += 1
+    # return the result
+    return result
+
+#Main program
+
+
