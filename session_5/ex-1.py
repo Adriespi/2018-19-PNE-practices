dc7b0d851c78186a6da18173384194e3dcd26bd9
adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-02-05 15:35:32 +0100

    Session 5 exercices

--- session_5/ex-1.py
+++ session_5/ex-1.py
@@ -4,11 +4,22 @@ def count_a(seq):
     #Counter for the A's
     result = 0
     for b in seq:
-        if b = 'A':
+        if b == 'A':
             result += 1
     # return the result
     return result
 
 #Main program
 
+s = 'AGTAGCCTAGATA'
+na = count_a(s)
+print('The number of As in the sequence is: {}'.format(na))
 
+#calculate the total sequence length
+tl = len(s)
+
+#calculate the percentage of As in the sequence
+perc = round(100.0 * na / tl,1)
+
+print('The total length is: {}'.format(tl))
+print('The total percentage of As is: {}%'.format(perc))
