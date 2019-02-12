f32e4571b872b8f9c14caee2cd4322e8fad05fcd
adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-02-12 16:51:04 +0100

    creating classes, objects, methods and working with them

---
+++ session_7/EXERCISE_2.py
@@ -0,0 +1,16 @@
+import socket
+
+#creating a socket or communicating with the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#same behaviour as a file
+PORT = 8080
+IP = '212.128.253.64'
+
+#connect to the server
+s.connect((IP,PORT))
+
+#send a message to the server
+while True:
+    msg = input('Enter: ')
+    s.send(str.encode(msg))
+
