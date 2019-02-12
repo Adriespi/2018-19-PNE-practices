d993e646fe072be90adf96048e09af162f969944
adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-02-12 16:37:53 +0100

    creating classes, objects, methods and working with them

--- session_7/client.py
+++ session_7/client.py
@@ -6,3 +6,19 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
 #same behaviour as a file
+PORT = 8080
+IP = '212.128.253.64'
+
+#connect to the server
+s.connect((IP,PORT))
+
+#send a message to the server
+s.send(str.encode('HELLO FROM MY CLIENT')) #encode is to translate the string into bytes
+
+#receive a message from the server
+msg = s.recv(2048).decode('utf-8')
+print('Message from the server: ')
+print(msg)
+s.close()
+
+print('The end')
