78c53ef6ca9de9211e19da897d4d9730e4925a68
adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-02-13 17:30:52 +0100

    creating classes, objects, methods and working with them

---
+++ P2/EXERCISE_3.py
@@ -0,0 +1,23 @@
+import socket
+
+#creating a socket or communicating with the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+#same behaviour as a file
+PORT = 8086
+IP = '212.128.253.88'
+while True:
+#connect to the server
+    s.connect((IP,PORT))
+
+#send a message to the server
+    msg2 = input('Message: ')
+    s.send(str.encode(msg2)) #encode is to translate the string into bytes
+
+#receive a message from the server
+msg = s.recv(2048).decode('utf-8')
+print('Message from the server: ')
+print(msg)
+s.close()
+
+print('The end')
---
+++ P2/client_p2.py
@@ -0,0 +1,2 @@
+#creating a client to connect to the teacher's server (create a socket to communicate o the main server
+
