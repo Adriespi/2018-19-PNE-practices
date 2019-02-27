f8c79797df94e3b9ac7f6a4e3c1a1de0fddd4b48
Adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-02-27 19:43:44 +0100

    Client-server connection

--- P2/local_server.py
+++ P2/local_server.py
@@ -1,51 +1,19 @@
 import socket
+# Create a socket for communicating with the server
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    PORT = 8086
+    IP = "212.128.253.81"
+    s.connect((IP, PORT))
+    from Seq import Seq
+    seq1 = Seq(input("Please, enter here your sequence : "))
+    seq2 = Seq(seq1.reverse())
+    seq3 = Seq(seq2.complement())
 
-# Configure the Server's IP and PORT
-PORT = 8085
-IP = "212.128.253.89"
-MAX_OPEN_REQUESTS = 5
+    s.send(str.encode(seq3.strbases))
 
-# Counting the number of connections
-number_con = 0
+    msg = s.recv(2048).decode("utf-8")
+    print("This is the messege from the server")
+    print(msg)
 
-# create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    serversocket.bind((IP, PORT))
-    # become a server socket
-    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
-    serversocket.listen(MAX_OPEN_REQUESTS)
-
-    while True:
-        # accept connections from outside
-        print("Waiting for connections at {}, {} ".format(IP, PORT))
-        (clientsocket, address) = serversocket.accept()
-
-        # Another connection!e
-        number_con += 1
-
-        condition = True
-        while condition:
-            # Print the conection number
-            print("CONNECTION: {}. From the IP: {}".format(number_con, address))
-
-            # Read the message from the client, if any
-            msg = clientsocket.recv(2048).decode("utf-8")
-            print("Message from client: {}".format(msg))
-
-            msg = msg.upper()
-            if msg == 'EXIT':
-                condition = False
-                clientsocket.close()
-            else:
-                # Send the message
-                message = input('TYPE: ')
-                send_bytes = str.encode(message)
-                # We must write bytes, not a string
-                clientsocket.send(send_bytes)
-except socket.error:
-    print("Problems using port {}. Do you have permission?".format(PORT))
-
-except KeyboardInterrupt:
-    print("Server stopped by the user")
-serversocket.close()
+    s.close()
