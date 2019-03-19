6094deef1800e76746e5a6edba67cc663e77094b
adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-03-19 15:28:44 +0100

    Json

--- P7/Session_17/test-json.py
+++ P7/Session_17/test-json.py
@@ -7,3 +7,7 @@ termcolor.cprint('Name:','green',end='')
 print(person['Firstname'],person['Lastname'])
 termcolor.cprint('Age:', 'green', end='')
 print(person['age'])
+
+for num in enumerate(person['Phonenumber']):
+    termcolor.cprint("Phone:",'blue',end='')
+    print(num)
